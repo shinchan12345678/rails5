fa67d79daa7057ed526d4a78dc700ee525d40bbc
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-06-04 08:56:42 +0900

    chapter1-4

--- chapter1-1.rb
+++ chapter1-1.rb
@@ -1,9 +1,33 @@
+# class User
+#   def name=(name)
+#     @name=name
+#   end
+
+#   def name
+#     @name
+#   end
+# end
+
 class User
-  def name=(name)
+  attr_accessor :name ,:address ,:email
+
+  def initialize(name,address,email)
     @name=name
+    @address=address
+    @email=email
   end
 
-  def name
-    @name
+  def profile
+    "#{name}(#{address})"
+  end
+
+  def name_call(full: true,with_email: true)
+    n = if full
+        "#{name}"
+        else
+          profile
+        end
+    n << "(#{email})" if with_email
   end
 end
+
