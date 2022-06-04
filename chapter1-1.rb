56c7d878ed48e68977232e35a6c10f8eefce8335
shinchan12345678 <102851643+shinchan12345678@users.noreply.github.com>
2022-06-04 09:43:43 +0900

    complete

--- chapter1-1.rb
+++ chapter1-1.rb
@@ -8,26 +8,63 @@
 #   end
 # end
 
-class User
-  attr_accessor :name ,:address ,:email
+# class User
+#   attr_accessor :name ,:address ,:email
 
-  def initialize(name,address,email)
-    @name=name
-    @address=address
-    @email=email
+#   def initialize(name,address,email)
+#     @name=name
+#     @address=address
+#     @email=email
+#   end
+
+#   def profile
+#     "#{name}(#{address})"
+#   end
+
+#   def name_call(full: true,with_email: true)
+#     n = if full
+#         "#{name}"
+#         else
+#           profile
+#         end
+#     n << "(#{email})" if with_email
+#   end
+# end
+
+class Tax
+  def self.rate
+    1.1
+  end
+end
+
+class PricedObject
+  def total_price
+    price *Tax.rate
   end
 
-  def profile
-    "#{name}(#{address})"
+  def price
+    raise NotImplementedError
+  end
+end
+
+class Product
+  attr_accessor :price
+
+  # def total_price
+  #   price*Tax.rate
+  # end
+end
+
+class OrderedItem
+  attr_accessor :unit_price, :volume
+
+  # 税抜価格*数量
+  def price
+    unit_price*volume
   end
 
-  def name_call(full: true,with_email: true)
-    n = if full
-        "#{name}"
-        else
-          profile
-        end
-    n << "(#{email})" if with_email
-  end
+  # def total_price
+  #   price*Tax.rate
+  # end
 end
 
